dd62c73ef5871af9a0e6a2e4c62a0c3ac58d8d03
Sean Pollock <seanpollock3344@gmail.com>
2017-02-21 12:13:18 -0600

    Messing around with nanomsg library.

--- ethereum-ipc-rust/src/main.rs
+++ ethereum-ipc-rust/src/main.rs
@@ -1,5 +1,33 @@
 extern crate nanomsg;
 
+use nanomsg::{Socket, Protocol, Error};
+
+fn create_socket() -> Result<(), Error> {
+    let mut socket = try!(Socket::new(Protocol::Pull));
+
+    let mut endpoint = try!(socket.bind("ipc:///tmp/pipeline.ipc"));
+
+    let mut msg = String::new();
+    loop {
+        try!(socket.read_to_string(&mut msg));
+        println!("We got a message: {}", &*msg);
+        msg.clear();
+    }
+
+    Ok(())
+}
+
+
+
+fn pusher() -> Result<(), Error> {
+    let mut socket = try!(Socket::new(Protocol::Push));
+    let mut endpoint = try!(Socket.connect("ipc:///tmp/pipeline.ipc"));
+
+    socket.write(b"messag in a bottle...");
+
+    endpoint.shutdown();
+    Ok(())
+}
 
 fn main() {
     println!("Hello, world!");
---
+++ ethereum-ipc-rust/target/debug/deps/ethereum_ipc_rust-cb67797aece897b1.d
@@ -0,0 +1,5 @@
+/Users/seanpollock/Documents/ConsenSys/rust-lessons/ethereum-ipc-rust/target/debug/deps/ethereum_ipc_rust-cb67797aece897b1: src/main.rs
+
+/Users/seanpollock/Documents/ConsenSys/rust-lessons/ethereum-ipc-rust/target/debug/deps/ethereum_ipc_rust-cb67797aece897b1.d: src/main.rs
+
+src/main.rs:
